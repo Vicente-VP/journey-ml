40537598242533813bde0e1739a600c0cd9620d5
JorgeTerence <jorge.terence@outlook.com>
2025-09-30 15:43:17 -0300

    feat: improved conversion docs

--- pipelines/conversion/training.py
+++ pipelines/conversion/training.py
@@ -1,10 +1,8 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-import seaborn as sns
 from sklearn.metrics import ConfusionMatrixDisplay
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.preprocessing import OneHotEncoder
 from yellowbrick.classifier import ClassificationReport
 
 df = pd.read_csv("datasets/aggregated-analytics.csv")
@@ -24,16 +22,15 @@ print(KNN_model.score(x_test, y_test))
 # TODO: testar com árvore
 # TODO: gráfico com diferentes K neighbours
 
-y_pred = KNN_model.predict(y_test)
+y_pred = KNN_model.predict(x_test)
 
 model = KNeighborsClassifier(n_neighbors=3)
-visualizer = ClassificationReport(model, classes=["Não Logado", "Logado"], support=True)
+labels=["Não Comprou", "Comprou"]
+visualizer = ClassificationReport(model, classes=labels, support=True)
 
 visualizer.fit(x_train, y_train)
 visualizer.score(x_test, y_test)
 visualizer.show()
-
-labels=["Não Logado", "Logado"]
 
 ConfusionMatrixDisplay.from_predictions(y_test, y_pred, display_labels=labels)
 
